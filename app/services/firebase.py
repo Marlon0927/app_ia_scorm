79c1c383b375f8215c063c7a8a1f091b10416566
Marlon0927 <marlonr.gomezskonradlorenz.edu.co>
2026-05-09 00:41:30 -0500

    firebase

--- app/services/firebase.py
+++ app/services/firebase.py
@@ -1,38 +1,29 @@
 import os
 import json
 import firebase_admin
-
 from firebase_admin import credentials, firestore, storage
 
-# ======================================
-# RENDER -> variable de entorno
-# LOCAL -> archivo json
-# ======================================
+db = None
+bucket = None
 
-firebase_json = os.getenv("FIREBASE_CREDENTIALS")
+try:
+    firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
 
-if firebase_json:
+    if not firebase_credentials:
+        raise Exception("FIREBASE_CREDENTIALS no encontrada")
 
-    # ===== PRODUCCIÓN / RENDER =====
-    firebase_dict = json.loads(firebase_json)
+    cred_dict = json.loads(firebase_credentials)
 
-    firebase_dict["private_key"] = firebase_dict["private_key"].replace("\\n", "\n")
+    cred = credentials.Certificate(cred_dict)
 
-    cred = credentials.Certificate(firebase_dict)
-
-else:
-
-    # ===== LOCAL =====
-    cred = credentials.Certificate(
-        "app/proyecto-scorm-firebase-adminsdk-fbsvc-2fc2d9fb11.json"
-    )
-
-# evitar inicialización duplicada
-if not firebase_admin._apps:
     firebase_admin.initialize_app(cred, {
         "storageBucket": "proyecto-scorm.firebasestorage.app"
     })
 
-db = firestore.client()
+    db = firestore.client()
+    bucket = storage.bucket()
 
-bucket = storage.bucket()
+    print("✅ Firebase inicializado correctamente")
+
+except Exception as e:
+    print(f"❌ Error Firebase: {e}")
