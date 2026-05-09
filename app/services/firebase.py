f43172d22035b9b86bbe6919b3fc8d728f55ae09
Marlon0927 <marlonr.gomezskonradlorenz.edu.co>
2026-05-09 00:12:39 -0500

    firebase ok

--- app/services/firebase.py
+++ app/services/firebase.py
@@ -1,5 +1,6 @@
 import os
 import json
+import tempfile
 import firebase_admin
 
 from firebase_admin import credentials, firestore, storage
@@ -7,15 +8,25 @@ from firebase_admin import credentials, firestore, storage
 firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
 
 if not firebase_credentials:
-    raise ValueError("FIREBASE_CREDENTIALS no encontrada")
+    raise Exception("FIREBASE_CREDENTIALS no encontrada")
 
+# Convertir string -> dict
 firebase_dict = json.loads(firebase_credentials)
 
-cred = credentials.Certificate(firebase_dict)
+# Crear archivo temporal
+with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".json") as temp_file:
+    json.dump(firebase_dict, temp_file)
+    temp_file_path = temp_file.name
 
-firebase_admin.initialize_app(cred, {
-    "storageBucket": "proyecto-scorm.firebasestorage.app"
-})
+# Inicializar Firebase
+if not firebase_admin._apps:
+    cred = credentials.Certificate(temp_file_path)
+
+    firebase_admin.initialize_app(cred, {
+        "storageBucket": "proyecto-scorm.firebasestorage.app"
+    })
 
 db = firestore.client()
 bucket = storage.bucket()
+
+print("✅ Firebase inicializado correctamente")
